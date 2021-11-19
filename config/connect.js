f2b167c6f99956a803b2400ef4aa1d25ede68498
Vaishakh GK <vaishakhgk2006@gmail.com>
2021-11-19 06:04:45 +0530

    for localhost

--- config/connect.js
+++ config/connect.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 var state = { db: null };
 
 module.exports.connect = (done) => {
-    var url = "mongodb://localhost:27017/mydb";
+    var url = process.env.MONGODB_URL || "mongodb://localhost:27017/mydb";
     var dbname = 'chat';
 
     MongoClient.connect(url, function (err, db) {
